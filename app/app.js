97806915f3a14030fbf24c16517aec3d5b5233ef
jjs88 <jjs457@gmail.com>
2018-02-17 15:42:51 -0500

    testing msg

--- app/app.js
+++ app/app.js
@@ -3,8 +3,10 @@
 
   if(window.location.href === 'https://jjs88.github.io/pizza-delivery-app/') {
     //pass in the absolute path if on github since relative paths use the root not the project folder
+    console.log('github');
     app.init('https://jjs88.github.io/pizza-delivery-app//data/customers.json');
   } else {
+    console.log('local');
     app.init('../data/customers.json');
   }
   
